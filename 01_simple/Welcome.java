9b6da6e7c68d3809b51683d29f5b4e2ba261f1fb
hegde10122 <hegde10122@gmail.com>
2016-11-07 22:08:05 +0530

    changes made

---
+++ 01_simple/Welcome.java
@@ -0,0 +1,11 @@
+
+//Simple line text prinitng program to start with
+public class Welcome {
+
+//main method to execute the main program
+public static void main(String[] args){
+
+    System.out.println("Welcome Folks to Java programming");//A simple line printing program
+}//end method main
+
+}//end class Welcome
